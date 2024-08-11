367e8f3257c0f9196ead327f7d1911599f2fda63
Eduard Lepner <eduard@invarion.com>
2024-08-11 10:44:55 +0200

    add ngrok command, prevent pan reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { toObservable } from '@angular/core/rxjs-interop';
 import { filter, map, merge, switchMap } from 'rxjs';
 import { isNotNull } from './utils';
-import { toTouchObservable, TouchEventData, touchEvents, TouchEventType } from './gestures';
+import { fromHtmlElementEvent, toTouchObservable, TouchEventData, touchEvents, TouchEventType } from './gestures';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div style="height: 100vh; display: flex; flex-direction: column;">
+    <div style="height: 100svh; display: flex; flex-direction: column;">
       <div style="flex-grow: 1; background-color: cyan" #targetEl></div>
       <div>hello world</div>
     </div>
@@ -31,6 +31,13 @@ export class AppComponent {
       })
     ).subscribe(obj => {
       console.log('touch stuff', obj);
-    });;
+    });
+
+    toObservable(this.targetEl).pipe(
+      filter(isNotNull),
+      switchMap((ref) => fromHtmlElementEvent(ref.nativeElement, 'touchmove'))
+    ).subscribe(evt => {
+      evt.preventDefault();
+    });
   }
 }
--- src/app/gestures.ts
+++ src/app/gestures.ts
@@ -1,37 +1,46 @@
-import { fromEvent, map, Observable, tap } from 'rxjs';
+import { fromEvent, map, Observable } from 'rxjs';
 import { Vector } from './math';
-
 
 export interface TouchData {
   touchId: number;
   position: Vector;
 }
 
-export type TouchEventData = TouchData[];
+export type TouchEventData = {
+  currentTouches: TouchData[];
+  changedTouches: TouchData[]
+};
 
 export const touchEvents = ['touchstart', 'touchmove', 'touchend', 'touchcancel'] as const;
-export type TouchEventType = typeof touchEvents[number]
-export function toTouchObservable<T extends TouchEventType>(target: HTMLElement, evtType: T): Observable<TouchData[]> {
+export type TouchEventType = typeof touchEvents[number];
+
+export function toTouchObservable<T extends TouchEventType>(target: HTMLElement, evtType: T): Observable<TouchEventData> {
   return fromHtmlElementEvent(target, evtType).pipe(
-    tap((evt) => {
-      if (evtType === 'touchend') {
-        console.log('Native event', evt)
-      }
-    }),
     map((evt) => {
-      return Array.from(evt.touches).map((touch) => ({
-        touchId: touch.identifier,
-        position: getPosition(touch)
-      }))
-    }));
+      return {
+        changedTouches: Array.from(evt.changedTouches).map((touch) => ({
+          touchId: touch.identifier,
+          position: getPosition(touch)
+        })),
+        currentTouches: Array.from(evt.touches).map((touch) => ({
+          touchId: touch.identifier,
+          position: getPosition(touch)
+        }))
+      }
+    })
+  );
 }
 
+export function tap(target: HTMLElement) {
+  toTouchObservable(target, 'touchstart').pipe(
 
+  )
+}
 
 function getPosition(touch: Touch): Vector {
   return [touch.clientX, touch.clientY];
 }
 
-function fromHtmlElementEvent<K extends keyof HTMLElementEventMap>(target: HTMLElement, type: K): Observable<HTMLElementEventMap[K]> {
+export function fromHtmlElementEvent<K extends keyof HTMLElementEventMap>(target: HTMLElement, type: K): Observable<HTMLElementEventMap[K]> {
   return fromEvent(target, type) as any;
 }
